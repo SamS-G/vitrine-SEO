7a083b8474a1c1fb76760841079eeef0b15f244e
Samuel Guichardon <samuel.guichardon@icloud.com>
2025-02-03 14:18:34 +0100

    section sans texte

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,9 +8,8 @@
     <div class="separator-right"></div>
     <section class="section-wrapper">
         <div class="section-title">
-            <h3 class="h1">Services</h3>
-            <h4>Menuiserie, Dépannage, Multi-Services</h4>
-            <i class="fa-solid fa-screwdriver-wrench"></i>
+            <h3 class="h1"><span>S</span>ervices</h3>
+            <h4 class="h5">Menuiserie, Dépannage, Multi-Services</h4>
         </div>
         <div class="card-group">
             <x-card
@@ -48,5 +47,22 @@
         </div>
     </section>
     <div class="separator-left"></div>
+
+    <section class="section-wrapper">
+        <div class="section-title">
+            <h3 class="h1"><span>P</span>ourquoi choisir <span>JD</span> Travaux Services ?</h3>
+            <h4 class="h5">Un Service de Qualité, Un Interlocuteur Unique</h4>
+        </div>
+        <div class="row">
+            <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta mollitia suscipit vitae! Autem culpa deserunt dicta doloribus eveniet, ex excepturi expedita in iste nisi, non repellat saepe sed ut voluptates!</span>
+                <span>Error libero, molestias nemo nihil recusandae reprehenderit veniam. Alias consectetur deserunt illum inventore iure magnam magni modi nemo nesciunt officiis quisquam ratione sed, sequi, sint suscipit ut voluptate? Impedit, ut?</span>
+                <span>Atque delectus, iusto optio quae sunt temporibus! A at beatae cupiditate dicta doloremque ducimus earum eos excepturi, fugiat impedit laudantium, libero maxime nulla, officiis optio quaerat quibusdam reprehenderit sequi veniam.</span>
+                <span>Accusantium cumque eligendi eos eveniet inventore molestias quibusdam recusandae! Cupiditate dolorum est impedit incidunt iusto, natus quae quia reprehenderit saepe similique. Accusamus alias id laborum nihil nisi placeat quisquam voluptatum.</span>
+                <span>Accusamus accusantium ad adipisci amet asperiores cupiditate delectus ducimus eaque fugiat itaque laborum, molestiae nemo odit quia quis saepe sint tempora tempore unde velit vitae voluptatem, voluptates? Atque, reprehenderit, voluptate.</span>
+                <span>A aperiam consequuntur dignissimos explicabo hic inventore minima molestias nisi tenetur? Cupiditate doloribus, eligendi fugit in iste magnam natus, nostrum quas quibusdam quos repudiandae sapiente sequi similique, sint velit voluptatibus.</span>
+            </p>
+        </div>
+    </section>
+    <div class="separator-right"></div>
 @endsection
-{{--TODO / Section pk me choisir ? / footer / bottom banner--}}
+{{--TODO footer / bottom banner--}}
