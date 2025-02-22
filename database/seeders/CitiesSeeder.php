edd9ad3ad4c1e625dd16f61d22db7e3a94927db7
Samuel Guichardon <samuel.guichardon@icloud.com>
2025-02-22 11:06:01 +0100

    ajout nouveau seeders pour cities

--- database/seeders/CitiesSeeder.php
+++ database/seeders/CitiesSeeder.php
@@ -57,7 +57,139 @@ class CitiesSeeder extends Seeder
                     'place-2' => "La Chèvre Verte",
                     'place-3' => "Le Mas-du-Puit"
                 ])
-            ]
+            ],
+            [
+                'slug' => 'dagneux',
+                'name' => 'Dagneux',
+                'postal_code' => '01120',
+                'latitude' => 45.8475,
+                'longitude' => 5.0561,
+                'landmarks' => json_encode([
+                    'place-1' => "Rue de Genève",
+                    'place-2' => "Rue du Lavoir",
+                    'place-3' => "Avenue de la Gare"
+                ])
+            ],
+            [
+                'slug' => 'leyment',
+                'name' => 'Leyment',
+                'postal_code' => '01150',
+                'latitude' => 45.9,
+                'longitude' => 5.3,
+                'landmarks' => json_encode([
+                    'place-1' => "Rue de la Mairie",
+                    'place-2' => "Rue des Ecoles",
+                    'place-3' => "Route de Chazey"
+                ])
+            ],
+            [
+                'slug' => 'beynost',
+                'name' => 'Beynost',
+                'postal_code' => '01700',
+                'latitude' => 45.8333,
+                'longitude' => 5.0167,
+                'landmarks' => json_encode([
+                    'place-1' => "Rue Centrale",
+                    'place-2' => "Rue du Mollard",
+                    'place-3' => "Avenue de la Gare"
+                ])
+            ],
+            [
+                'slug' => 'miribel',
+                'name' => 'Miribel',
+                'postal_code' => '01700',
+                'latitude' => 45.8214,
+                'longitude' => 4.955,
+                'landmarks' => json_encode([
+                    'place-1' => "Grande Rue",
+                    'place-2' => "Rue des Ecoles",
+                    'place-3' => "Avenue de la Gare"
+                ])
+            ],
+            [
+                'slug' => 'montluel',
+                'name' => 'Montluel',
+                'postal_code' => '01120',
+                'latitude' => 45.85,
+                'longitude' => 5.05,
+                'landmarks' => json_encode([
+                    'place-1' => "Rue Notre Dame",
+                    'place-2' => "Rue de la Portelle",
+                    'place-3' => "Avenue Pierre Cormorèche"
+                ])
+            ],
+            [
+                'slug' => 'tramoyes',
+                'name' => 'Tramoyes',
+                'postal_code' => '01390',
+                'latitude' => 45.8833,
+                'longitude' => 5.95,
+                'landmarks' => json_encode([
+                    'place-1' => "Rue du Centre",
+                    'place-2' => "Rue des Ecoles",
+                    'place-3' => "Route de Mionnay"
+                ])
+            ],
+            [
+                'slug' => 'perouges',
+                'name' => 'Pérouges',
+                'postal_code' => '01800',
+                'latitude' => 45.9,
+                'longitude' => 5.1833,
+                'landmarks' => json_encode([
+                    'place-1' => "Rue des Rondes",
+                    'place-2' => "Rue du For",
+                    'place-3' => "Route du Prince"
+                ])
+            ],
+            [
+                'slug' => 'charnoz-sur-ain',
+                'name' => 'Charnoz-sur-Ain',
+                'postal_code' => '01800',
+                'latitude' => 45.8833,
+                'longitude' => 5.2333,
+                'landmarks' => json_encode([
+                    'place-1' => "Rue de la Mairie",
+                    'place-2' => "Rue des Ecoles",
+                    'place-3' => "Route de Loyes"
+                ])
+            ],
+            [
+                'slug' => 'chazey-sur-ain',
+                'name' => 'Chazey-sur-Ain',
+                'postal_code' => '01150',
+                'latitude' => 45.8833,
+                'longitude' => 5.25,
+                'landmarks' => json_encode([
+                    'place-1' => "Rue du Château",
+                    'place-2' => "Rue de la Mairie",
+                    'place-3' => "Route de Leyment"
+                ])
+            ],
+            [
+                'slug' => 'villieu-loyes-mollon',
+                'name' => 'Villieu-Loyes-Mollon',
+                'postal_code' => '01800',
+                'latitude' => 45.9167,
+                'longitude' => 5.2167,
+                'landmarks' => json_encode([
+                    'place-1' => "Grande Rue",
+                    'place-2' => "Rue de la Mairie",
+                    'place-3' => "Route de Loyes"
+                ])
+            ],
+            [
+                'slug' => 'la-balme-les-grottes',
+                'name' => 'La-Balme-Les-Grottes',
+                'postal_code' => '38390',
+                'latitude' => 45.85,
+                'longitude' => 5.3333,
+                'landmarks' => json_encode([
+                    'place-1' => "Rue des Grottes",
+                    'place-2' => "Rue de la Mairie",
+                    'place-3' => "Route de Hières-sur-Amby"
+                ])
+            ],
         ]);
     }
 }
