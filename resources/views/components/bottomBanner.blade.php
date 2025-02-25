4d9ec239a885d14e976955c3ebb06f461df97bd8
Samuel Guichardon <samuel.guichardon@icloud.com>
2025-02-25 20:51:52 +0100

    oubli 1 image

--- resources/views/components/bottomBanner.blade.php
+++ resources/views/components/bottomBanner.blade.php
@@ -1,4 +1,10 @@
 <div class="bottom-banner">
-    <img class="img-fluid" src="{{ Agent::isMobile() ? Vite::asset('resources/images/responsive/home/bottom-banner.webp') : Vite::asset('resources/images/home/bottom-banner.webp')}}" alt="table avec outillage de menuisier">
+    <img class="img-fluid"
+         src="{{ Agent::isMobile() ? Vite::asset('resources/images/responsive/home/bottom-banner.webp') : Vite::asset('resources/images/home/bottom-banner.webp')}}"
+         alt="table avec outillage de menuisier"
+         title="table avec outillage pour faire de la menuiserie"
+         width="2533"
+         height="779"
+    >
 </div>
 
